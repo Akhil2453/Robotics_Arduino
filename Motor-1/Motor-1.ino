eddf419133f30e17cc78ebe7f64be900376f97d1
Akhil <akhil.menon94@gmail.com>
2017-12-30 10:43:42 +0530

    Motor-1 --> motor interfacing without motor driver

---
+++ Motor-1/Motor-1.ino
@@ -0,0 +1,35 @@
+/*
+Motor Interfacing
+
+Interface a DC mtor with Arduino
+(without Motor Driver)
+
+This can be done in two ways:
+ 1. This code shows the first method:
+    Connections ::
+    the two terminals(wire||poles||points) of motors are : 
+    m1 AND m2
+
+    m1 --> pin 13
+    m2 --> Gnd
+ */
+
+//initialise a variable with pin 13
+int m1 = 13;
+void setup() {
+  // put your setup code here, to run once:
+  // Make the pin as Output
+  pinMode(m1, HIGH);
+  // can also be written as pinMode(pin, 1) OR pinMode(pin, OUTPUT)
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // Motor will run in 1 second intervals
+  digitalWrite(m1, 1);
+  delay(1000);
+  digitalWrite(m1, 0);
+  delay(1000);
+}
